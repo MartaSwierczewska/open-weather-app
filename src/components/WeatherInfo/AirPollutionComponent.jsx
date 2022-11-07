2928c8e23427813ea7397e6c03ef8028f02a05c9
MartaSwierczewska <martaswierczewska@wp.pl>
2022-11-07 15:46:09 +0100

    info about air components

--- src/components/WeatherInfo/AirPollutionComponent.jsx
+++ src/components/WeatherInfo/AirPollutionComponent.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import {AiOutlineInfoCircle} from 'react-icons/ai';
+import {getAirComponentUrl} from "../../services/OpenWeatherService";
 import './WeatherInfo.css';
 
 function AirPollutionComponent({component, isDataAvailable}) {
 
+    const handleClick = (componentName) => {
+        window.open(getAirComponentUrl(componentName), "_blank")
+    };
+
     if (isDataAvailable) {
         return (
             <div style={{marginBottom: "20px"}}>
-                <AiOutlineInfoCircle className="air-pollution-info"/>
+                <AiOutlineInfoCircle className="air-pollution-info" onClick={() => handleClick(component[0])}/>
                 <span key={component[0]}>{component[0]}: <b>{component[1]}</b> μg/m3</span>
             </div>
         )
     } else {
         return (
             <div style={{marginBottom: "20px"}}>
-                <AiOutlineInfoCircle className="air-pollution-info"/>
+                <AiOutlineInfoCircle className="air-pollution-info" onClick={() => handleClick(component)}/>
                 <span key={component}>{component}: </span>
             </div>
         )
--- src/services/OpenWeatherService.js
+++ src/services/OpenWeatherService.js
@@ -9,7 +9,10 @@ export const extractComponents = (result) => {
     return Object.keys(airPollutionComponents).map((key) => [airComponentsMap.get(key), airPollutionComponents[key]]);
 }
 
-// TODO add more description
+export const getAirComponentUrl = (component) => {
+  return airComponentsUrlMap.get(component);
+}
+
 export const transformAirQualityIndex = (index) => {
     switch(index) {
         case 1:
@@ -64,5 +67,16 @@ const airComponentsMap = new Map([
     ['pm2_5', 'Fine particles matter'],
     ['pm10', 'Coarse particulate matter'],
     ['nh3', 'Ammonia'],
+  ]);
+
+  const airComponentsUrlMap = new Map([
+    ['Carbon monoxide', 'https://en.wikipedia.org/wiki/Carbon_monoxide'],
+    ['Nitrogen monoxide', 'https://en.wikipedia.org/wiki/Nitric_oxide'],
+    ['Nitrogen dioxide', 'https://en.wikipedia.org/wiki/Nitrogen_dioxide'],
+    ['Ozone', 'https://en.wikipedia.org/wiki/Ozone'],
+    ['Sulphur dioxide', 'https://en.wikipedia.org/wiki/Sulfur_dioxide'],
+    ['Fine particles matter', 'https://en.wikipedia.org/wiki/Particulates'],
+    ['Coarse particulate matter', 'https://en.wikipedia.org/wiki/Particulates#Size,_shape_and_solubility_matter'],
+    ['Ammonia', 'https://en.wikipedia.org/wiki/Ammonia'],
   ]);
 
